349ea4c9c4ea6da9ca2678262d5afd676850e20a
otakbeku <iceramngl@gmail.com>
2016-09-21 02:33:20 +0700

    Add project files.

---
+++ Tugas_ppk_1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tugas_ppk_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            int x = Convert.ToInt16(textBox1.Text);
+            int y = Convert.ToInt16(textBox2.Text);
+
+            int hasil = x + y;
+
+            textBox3.Text = Convert.ToString(hasil);
+
+        }
+    }
+}
